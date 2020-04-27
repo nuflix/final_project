de7b9d429eaa25dff1240dfeb18ad2e0bf9e5da9
Nuflix <djordje.vujicic@yandex.com>
2020-04-27 22:02:40 +0200

    search pages

--- scripts/search.js
+++ scripts/search.js
@@ -1,10 +1,12 @@
 document.getElementById("searchButton").addEventListener("click", apiSearch);
 let searchAllProducts = [];
-let $searchByName="act";
+let searchByName="";
 let apiKey = "IcvHwuaZZxwGBTQF0z3zXAcI";
 function apiSearch(){
 
-    fetch(`https://api.bestbuy.com/v1/products(releaseDate<=today&name=\"${$searchByName}*\")?apiKey=${apiKey}&format=json&show=sku,name,customerReviewAverage,customerReviewCount,regularPrice,salePrice,longDescription,shortDescription,image,regularPrice,salePrice,releaseDate,type&pageSize=9&page=1,customerReviewAverage&sort=releaseDate.dsc`)
+searchByName=document.getElementById("searchText").value;
+
+    fetch(`https://api.bestbuy.com/v1/products(releaseDate<=today&name=\"${searchByName}*\")?apiKey=${apiKey}&format=json&show=sku,name,customerReviewAverage,customerReviewCount,regularPrice,salePrice,longDescription,shortDescription,image,regularPrice,salePrice,releaseDate,type&pageSize=9&page=1,totalPages,customerReviewAverage&sort=releaseDate.dsc`)
     .then((response) => {
         return response.json();
       })
@@ -14,10 +16,23 @@ function apiSearch(){
 
         //here we call functions that need argument product list
         searchAllProducts = productsLocal.products;
-        console.log(searchAllProducts);
+        search(productsLocal);
         return 0;
 
       }).catch(error => alert(error.toString()));
   
+
+    }
+
+    function search(x){
+      for(let i=0; i<x.products.length; i++){
+        document.getElementById("main").innerHTML += `<br>
+        ${i}${x.products[i].name}
+        `;
+      }
+
+      if(x.totalPages>3){
+        document.getElementById("pageList").innerHTML = `<div class="pageBtn">1</div><div class="pageBtn">2</div><div class="pageBtn">3</div>...<div class="pageBtn">${x.totalPages}</div>`;
+      }
 
     }
