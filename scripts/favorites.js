fdda7122609fb80abbbc115468385a088f9aa7cb
Nuflix <djordje.vujicic@yandex.com>
2020-04-30 19:07:28 +0200

    favorites finished, errors

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -3,6 +3,7 @@ let count = 0;
 
 document.getElementById("content").addEventListener("click", removeFav);
 
+
 function loadFavApi(i){
     
     fetch(`https://api.bestbuy.com/v1/products/${arr[i]}.json?show=sku,image,name,salePrice&apiKey=IcvHwuaZZxwGBTQF0z3zXAcI`)
@@ -18,7 +19,7 @@ function loadFavApi(i){
            `;
             }
         document.getElementsByClassName("sliderForTopRated")[document.getElementsByClassName("sliderForTopRated").length-1].innerHTML+=`
-        <div class="topRatedSection"><div class="hiddenJsonForDetails">${JSON.stringify(data)}</div><div class="sliderPageForTopRated"><img src="${data.image}"><div class="sliderPageInvisibleLayer"><h3 class="sliderPageTitle">${data.name}</h3></div></div><div class="topRatedFooter"><div class="topRatedPrice">${data.salePrice}$</div><div class="sliderDetailsBtn">Details</div><div class="sliderDetailsBtn removeFav" data-id="${arr[i]}">Remove</div></div></div>
+        <div class="topRatedSection"><div class="hiddenJsonForDetails">${JSON.stringify(data)}</div><div class="sliderPageForTopRated"><img src="${data.image}"><div class="sliderPageInvisibleLayer"><h3 class="sliderPageTitle">${data.name}</h3></div></div><div class="topRatedFooter"><div class="topRatedPrice">${data.salePrice}$</div><div class="sliderDetailsBtn details">Details</div><div class="sliderDetailsBtn removeFav" data-id="${arr[i]}">Remove</div></div></div>
         `;
         count++;
         if(count<arr.length){
@@ -43,6 +44,10 @@ function removeFav(){
         arr = arr.filter(e => e !== event.target.dataset.id);
         localStorage.setItem("favorites", JSON.stringify(arr));
         location.reload();
+    }
+
+    if(event.target.classList.contains("details")){
+        window.modalWindowShow(JSON.parse(event.target.parentNode.parentNode.getElementsByClassName("hiddenJsonForDetails")[0].innerHTML));
     }
 }
 
