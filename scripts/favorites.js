7f1e65f695eb392dfee84f910eb30d72a484bbeb
Nuflix <djordje.vujicic@yandex.com>
2020-04-30 18:53:19 +0200

    favorites complete

---
+++ scripts/favorites.js
@@ -0,0 +1,49 @@
+let arr=[];
+let count = 0;
+
+document.getElementById("content").addEventListener("click", removeFav);
+
+function loadFavApi(i){
+    
+    fetch(`https://api.bestbuy.com/v1/products/${arr[i]}.json?show=sku,image,name,salePrice&apiKey=IcvHwuaZZxwGBTQF0z3zXAcI`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+        if(i%3===0){
+            document.getElementById("topRatedContent").innerHTML+=`
+            <div class="sliderForTopRated">
+           
+            </div>
+           `;
+            }
+        document.getElementsByClassName("sliderForTopRated")[document.getElementsByClassName("sliderForTopRated").length-1].innerHTML+=`
+        <div class="topRatedSection"><div class="hiddenJsonForDetails">${JSON.stringify(data)}</div><div class="sliderPageForTopRated"><img src="${data.image}"><div class="sliderPageInvisibleLayer"><h3 class="sliderPageTitle">${data.name}</h3></div></div><div class="topRatedFooter"><div class="topRatedPrice">${data.salePrice}$</div><div class="sliderDetailsBtn">Details</div><div class="sliderDetailsBtn removeFav" data-id="${arr[i]}">Remove</div></div></div>
+        `;
+        count++;
+        if(count<arr.length){
+        loadFavApi(count);
+        }
+    });
+
+}
+
+function prepareFav(){
+    if(localStorage.getItem("favorites")!==null){
+        if(JSON.parse(localStorage.getItem("favorites")).length>0){
+        arr=JSON.parse(localStorage.getItem("favorites"));
+        loadFavApi(0);
+        }
+    }
+}
+
+
+function removeFav(){
+    if(event.target.classList.contains("removeFav")){
+        arr = arr.filter(e => e !== event.target.dataset.id);
+        localStorage.setItem("favorites", JSON.stringify(arr));
+        location.reload();
+    }
+}
+
+prepareFav();
--- scripts/index.js
+++ scripts/index.js
@@ -19,12 +19,33 @@ ratings = "Average rating: " + productCharacteristics.customerReviewAverage + " 
 }else{
 ratings = "No ratings for this product." 
 }
-document.getElementById("modalWindowContent").innerHTML = `<img class="modalWindowImage" src="${productCharacteristics.image}" /><div class="modalWindowString">name: ${productCharacteristics.name}</div><div class="modalWindowString">Release date: ${productCharacteristics.releaseDate}</div><div class="modalWindowString">${ratings}</div><div class="modalWindowString">Regular price: ${productCharacteristics.regularPrice}</div><div class="modalWindowString">Sale price: ${productCharacteristics.salePrice}</div><div><div class="buyBtn">Buy!</div><div class="sliderDetailsBtn addToFavBtn">Add to favorites</div></div><div class="modalWindowString">description: ${productCharacteristics.longDescription}</div><div id="sku" class="invisible">${productCharacteristics.sku}</div>`;
+document.getElementById("modalWindowContent").innerHTML = `<img class="modalWindowImage" src="${productCharacteristics.image}" /><div class="modalWindowString">name: ${productCharacteristics.name}</div><div class="modalWindowString">Release date: ${productCharacteristics.releaseDate}</div><div class="modalWindowString">${ratings}</div><div class="modalWindowString">Regular price: ${productCharacteristics.regularPrice}</div><div class="modalWindowString">Sale price: ${productCharacteristics.salePrice}</div><div><div class="buyBtn">Buy!</div><div id="addToFavBtn" class="sliderDetailsBtn addToFavBtn">Add to favorites</div></div><div class="modalWindowString">description: ${productCharacteristics.longDescription}</div><div id="sku" class="invisible">${productCharacteristics.sku}</div>`;
+
+document.getElementById("addToFavBtn").addEventListener("click", addToFavorites);
 }
 
 function closeModalWindow(){
   document.getElementById("modalWindowContainer").classList.add("invisible");  
   window.sliderMouseOver = 0;
+}
+
+
+function addToFavorites(){
+  let sku = document.getElementById("sku").innerHTML;
+  if (localStorage.getItem("favorites") === null) {
+    let arr=[];
+    arr.push(sku);
+    localStorage.setItem("favorites", JSON.stringify(arr));
+  }else{
+    let arr = JSON.parse(localStorage.getItem("favorites"));
+    console.log(arr);
+    if(arr.includes(sku)){
+    alert("Item already in favorites");
+    }else{
+      arr.push(sku);
+      localStorage.setItem("favorites", JSON.stringify(arr));
+    }
+  }
 }
 
 /* api(); */
